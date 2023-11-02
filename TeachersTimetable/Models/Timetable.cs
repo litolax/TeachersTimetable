9abec954b53567334690b41246f52fc8e39bd7bc
wh0o7 <andruxakosinec7@gmail.com>
2023-11-02 16:23:56 +0300

    Add last state feature

--- TeachersTimetable/Models/Timetable.cs
+++ TeachersTimetable/Models/Timetable.cs
@@ -5,7 +5,6 @@ namespace TeachersTimetable.Models;
 
 public class Timetable
 {
-    public ObjectId Id { get; set; }
     public string Date { get; set; } = "";
     [BsonIgnore] public List<TeacherInfo> TeacherInfos { get; set; } = new();
 }
